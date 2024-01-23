d3b6514da3f54213e7c069e752a4913da8f634af
Naraganesh <naraganesh143@gmail.com>
2024-01-23 22:30:19 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from app.forms import * 
-from django.http import HttpResponse 
-
+from django.http import HttpResponse,HttpResponseRedirect 
+from django.urls import reverse
+from django.core.mail import send_mail 
+from django.contrib.auth import authenticate,login
 
 def registration(request):
     ufo=UserForm() 
@@ -22,8 +24,15 @@ def registration(request):
 
             MPFDO=pfd.save(commit=False)
             MPFDO.username=MUFDO
-            MPFDO.save() 
+            MPFDO.save()   
 
+            send_mail('registration',
+                      'thank u registraion is successfully done',
+                      'naraganesh143@gmail.com',
+                      [MUFDO.email],
+                      fail_silently=True)
+
+           
             return HttpResponse('registartion is successfully')  
         else:
             return HttpResponse('invalid')
@@ -31,3 +40,31 @@ def registration(request):
 
     return render(request,'registration.html',d)
 
+ 
+
+def home(request): 
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username} 
+        return render(request,'home.html',d)
+    return render(request,'home.html') 
+
+
+
+
+
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['un']
+        password=request.POST['pw']
+        AUO=authenticate(username=username,password=password)
+
+        if AUO and AUO.is_active:
+            login(request,AUO)
+            request.session['username']=username
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            return HttpResponse('Invalid Credentials')
+
+    return render(request,'user_login.html')
